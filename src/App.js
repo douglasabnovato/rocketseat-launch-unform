8fae66171c792beb9c1da7008f3a27aeb1d35a06
@douglasabnovato <douglasabnovato@gmail.com>
2019-05-16 18:30:11 -0300

    rocketseat launch #1 - unform

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as Yup from 'yup'; 
+import { Form, Scope, Input } from '@rocketseat/unform';
+
+const initialData = { 
+  name: 'Diego',
+  address: {
+    street: 'Rua dos Artistas',
+  },
+};
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Campo Obrigatório'),
+  address: Yup.object().shape({
+    number: Yup.string().min(3, 'Número precisa de 3 dígitos').required('Campo Obrigatório')
+  })
+});
 
 function App() {
+  function handleSubmit(data){
+    console.data();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form schema={schema} initialData={initialData} onSubmit={handleSubmit}>
+      <Input name="name" label="Nome: "/><br/>
+      <Scope path="address">
+        <Input name="street" label="Rua:"/><br/>
+        <Input name="number" label="Número:"/><br/>
+      </Scope>
+      <button type="submit">Enviar</button>
+    </Form>    
   );
 }
 
